428d799c9be180d28b467d3bd6b4c9e1a91d0e9e
NSR <narcishow1@hotmail.com>
2024-06-14 20:39:05 +0200

    changes in login

--- src/components/StarPage/Forms/Login.jsx
+++ src/components/StarPage/Forms/Login.jsx
@@ -1,69 +1,31 @@
 import { Button, FormControl, FormLabel, Input, Stack, useToast } from '@chakra-ui/react';
-import { useEffect, useRef, useState } from "react";
+import {  useRef  } from "react";
 import { useNavigate } from 'react-router';
 import { handleSubmitLogin } from '../../../reducer/StartPageReducer/startPage.action';
-import debounce from 'lodash.debounce';
+
 
 const Login = ({dispatch, isLoadingLogin, isButtonDisabledLogin }) => {
     const toast = useToast();
     const navigate = useNavigate();
     const userNameRef = useRef(null);
     const passwordRef = useRef(null);
-    const lastToastTimeRef = useRef(0);
-    const [debounceTimer, setDebounceTimer] = useState(null);
-
-    const showToast = () => {
-        toast({
-            title: "User y password deben tener min de 5 letras y una mayúscula al principio",
-            status: "error",
-            duration: 1500,
-            isClosable: true,
-        });
-        lastToastTimeRef.current = Date.now();
-    };
    
-    const handleChange = () => {
+    const handleChangeInputLogin = () => {
         const userName = userNameRef.current.value;
         const password = passwordRef.current.value;
 
-        const now = Date.now();
-        const debounceInterval = 5000; // Interval after which the toast can reappear
-
-        if (userName.length < 5 || !(/^(?=.*[A-Z]).{5,}$/.test(password))) {
-            if (now - lastToastTimeRef.current > debounceInterval) {
-                showToast();
-            }
-        }
-
         if (userName.length >= 5 && /^(?=.*[A-Z]).{5,}$/.test(password)) {
             dispatch({ type: "IS_LOGIN_BUTTON", payload: false });
-            if (debounceTimer) {
-                clearTimeout(debounceTimer);
-                setDebounceTimer(null);
-            }
+          
         } else {
             dispatch({ type: "IS_LOGIN_BUTTON", payload: true });
-            if (!debounceTimer) {
-                setDebounceTimer(setTimeout(() => {
-                    if (userName.length < 5 || !(/^(?=.*[A-Z]).{5,}$/.test(password))) {
-                        showToast();
-                    }
-                }, debounceInterval));
-            }
+         
         }
     };
 
-    useEffect(() => {
-        return () => {
-            if (debounceTimer) {
-                clearTimeout(debounceTimer);
-            }
-        };
-    }, [debounceTimer]);
-
     return (
         <>
-             <Stack border='3px solid black' 
+             <Stack border='3px solid black' width="100%"
              padding='var(--nsr-padding2)' 
              borderRadius='var(--nsr-bradius2)' gap="var(--nsr-gap1)" 
              background="linear-gradient(135deg, var(--nsr-color19), var(--nsr-color20))">
@@ -73,14 +35,23 @@ const Login = ({dispatch, isLoadingLogin, isButtonDisabledLogin }) => {
              color="var(--nsr-color1)">
                  <FormLabel htmlFor="userName">User: </FormLabel>
                  <Input id="userName" type="text" placeholder='Debe tener 5 letras min' 
-                 isRequired ref={userNameRef} onChange={handleChange} title='Debe tener 5 letras o mas..'/>
+                 isRequired ref={userNameRef} onChange={handleChangeInputLogin} title='Debe tener 5 letras o mas..'/>
+                 {(userNameRef?.current?.value.length < 5 && userNameRef.current.value !== "") && (
+                     <p style={{ color: "var(--nsr-color3)", fontWeight: "bold", 
+                        marginTop:"var(--nsr-margin1)", fontSize:"11px" }}>Min 5 letras....</p>
+                 ) }
                  
                  <FormLabel htmlFor="password" marginTop="var(--nsr-margin2)">Password: </FormLabel>
                  <Input id="password" type="password" placeholder='5 letras y una mayúscula' pattern="^(?=.*[A-Z]).{5,}$"
                      title="Debe contener al menos 5 letras y una mayúscula" isRequired ref={passwordRef} 
                      onChange={()=>{
-                        handleChange()
+                        handleChangeInputLogin()
                         }} />
+                    {(passwordRef?.current?.value.length < 5 && passwordRef.current.value !== "") && (
+                    <p style={{ color: "var(--nsr-color3)", fontWeight: "bold", fontSize:"12px", marginTop:"var(--nsr-margin1)"  }}>
+                        1ª con mayúscula y min 5 letras...
+                    </p>
+                    )}
                  <Button
                      bgGradient="linear(to-br, #f9eb0a, #ec008c, #005caf)"
                      marginTop="var(--nsr-margin2)"
